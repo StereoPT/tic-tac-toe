cbb5496ef90d789943a4456724799765290d416a
Guido <stereopt@gmail.com>
2023-03-18 23:36:49 +0000

    feat: board from array

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,19 +1,17 @@
+import { useState } from "react";
+
 import Cell from "../Cell/Cell";
 
+const initialBoardState = new Array(9).fill(0);
+
 const Board = () => {
+  const [board, setBoard] = useState(initialBoardState);
+
   return (
     <div className="grid gap-4 grid-cols-3 grid-rows-3">
-      <Cell text="O" />
-      <Cell text="X" />
-      <Cell text="O" />
-
-      <Cell text="X" />
-      <Cell text="X" />
-      <Cell text="O" />
-
-      <Cell text="O" />
-      <Cell text="O" />
-      <Cell text="X" />
+      {board.map((cell) => {
+        return <Cell text={cell} />;
+      })}
     </div>
   );
 };
