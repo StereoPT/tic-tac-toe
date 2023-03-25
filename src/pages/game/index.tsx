3f109e282a8907d5c2fe2af962f650bb9f9c2693
Guido <stereopt@gmail.com>
2023-03-25 14:30:07 +0000

    feat: home page

---
+++ src/pages/game/index.tsx
@@ -0,0 +1,11 @@
+import Board from "@/components/Board/Board";
+
+const TicTacToe = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Board />
+    </div>
+  );
+};
+
+export default TicTacToe;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
-import Board from "@/components/Board/Board";
+import Link from "next/link";
 
-const TicTacToe = () => {
+const HomePage = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <Board />
+      <div className="flex items-center justify-center flex-col gap-8">
+        <span className="text-6xl font-bold">Tic-Tac-Toe</span>
+        <Link href="/game">
+          <span className="px-4 py-4 bg-slate-600 text-white rounded-lg text-xl">Start Game</span>
+        </Link>
+      </div>
     </div>
   );
 };
 
-export default TicTacToe;
+export default HomePage;
