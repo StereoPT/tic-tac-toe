f4733188aff4890d0424ae8b513e40eb2182fb3b
Guido <stereopt@gmail.com>
2023-04-26 13:10:10 +0100

    fix: backend dist

--- backend/dist/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFound = (req, res, next) => {
-    const error = new Error(`Not Found :: ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-};
-exports.default = notFound;
-//# sourceMappingURL=notFound.js.map
--- backend/dist/sockets/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ServerSocket = (socket) => {
-    console.log(" - User Connected!");
-};
-exports.default = ServerSocket;
-//# sourceMappingURL=index.js.map
